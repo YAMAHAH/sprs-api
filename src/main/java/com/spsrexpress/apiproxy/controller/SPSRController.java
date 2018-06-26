3bd31bebde3e510d2f5b5f2e2866171aaba50b89
YAMAHAH <2910112448@qq.com>
2018-06-26 11:23:32 +0800

    修复逻辑

--- src/main/java/com/spsrexpress/apiproxy/controller/SPSRController.java
+++ src/main/java/com/spsrexpress/apiproxy/controller/SPSRController.java
@@ -1,9 +1,11 @@
 package com.spsrexpress.apiproxy.controller;
 
+import com.spsrexpress.apiproxy.utils.DataMapUtil;
 import com.spsrexpress.apiproxy.utils.HttpRequestUtil;
 import com.spsrexpress.apiproxy.utils.XMLConvertUtil;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
@@ -15,6 +17,7 @@ import springfox.documentation.annotations.ApiIgnore;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/spsr/v1/")
@@ -57,6 +60,12 @@ public class SPSRController {
     @Value("${spsr.login.password}")
     private String password;
 
+    @Value("${spsr.login.secureId")
+    private String secureId;
+
+    @Value("${spsr.ContractNumber}")
+    private String contractNumber;
+
     /**
      * 可以直接使用@ResponseBody响应JSON
      *
@@ -75,22 +84,24 @@ public class SPSRController {
         return map;
     }
 
-    @ApiOperation(value = "用户登录",notes = "获取登录用户的SID")
-    @PostMapping(path = "/waLogin",produces = "application/json")
-    public String login() throws IOException{
+    private String login() throws IOException{
         String xml = "<root xmlns=\"http://spsr.ru/webapi/usermanagment/login/1.0\">\n"
                 + "<p:Params Name=\"WALogin\" Ver=\"1.0\" xmlns:p=\"http://spsr.ru/webapi/WA/1.0\" />\n"
                 + "<Login  Login=\"" + username + "\" Pass=\"" + password +  "\" UserAgent=\"Company name\" />\n" + "</root>";
 
         String res =httpRequestUtil.postRequest(getRequestUrl(), xml);
-        String jsonStr = xmlConvertUtil.xmlToJson(res);
-
-        return jsonStr;
+//        String jsonStr = xmlConvertUtil.xmlToJson(res);
+//        JSONObject jsonObject = xmlConvertUtil.xmlToJSONObject(res);
+//        xmlConvertUtil.getValue(jsonObject,"root.Result.RC");
+        return res;
     }
 
-    @ApiOperation(value="注销SID", notes="注销指定的SID")
-    @PostMapping(path="/waLogout",produces = "application/json")
-    public String logout(@RequestParam String sId) throws IOException{
+    @PostMapping(path="/waLogin",produces = "application/json")
+    public Map<String,Object> waLogin() throws IOException{
+        return xmlConvertUtil.xmlToMap(this.login());
+    }
+
+    private String logout(String sId) throws IOException{
         String reqParam = "<root xmlns=\"http://spsr.ru/webapi/usermanagment/logout/1.0\" >\n" +
                 " <p:Params Name=\"WALogout\" Ver=\"1.0\" xmlns:p=\"http://spsr.ru/webapi/WA/1.0\" />\n" +
                 " <Logout Login=\" " + username +  "\" SID=\" " + sId + "\" /> </root>";
@@ -119,8 +130,8 @@ public class SPSRController {
 
     @ApiOperation(value = "创建并激活快递单",notes = "根据指定的请求参数创建并激活快递单")
     @PostMapping(value = "/waCreateInvoice",produces = MediaType.APPLICATION_JSON_VALUE)
-    public String createInvoice(@ApiParam(name = "reqParam", value = "JSON或XML格式的参数数据", required = true)
-                                     @RequestBody String reqParam, HttpServletRequest request) throws IOException{
+    public String createInvoice2(@ApiParam(name = "reqParam", value = "JSON或XML格式的参数数据", required = true)
+                                @RequestBody String reqParam, HttpServletRequest request) throws IOException{
         String contentType = request.getHeader("Content-Type").toLowerCase();
 
         String res="{ \"result\": \"0\" }";
@@ -131,6 +142,37 @@ public class SPSRController {
             res = httpRequestUtil.postRequest(getRequestUrl(), reqXml);
         }
         return xmlConvertUtil.xmlToJson(res);
+    }
+
+    @ApiOperation(value = "创建并激活快递单",notes = "根据指定的请求参数创建并激活快递单")
+    @PostMapping(value = "/waCreateInvoice",produces = MediaType.APPLICATION_JSON_VALUE)
+    public String createInvoice(@ApiParam(name = "reqParam", value = "JSON或XML格式的参数数据", required = true)
+                                     @RequestBody String reqParam, HttpServletRequest request) throws IOException{
+        String contentType = request.getHeader("Content-Type").toLowerCase();
+
+        String res="{ \"result\": \"0\" }";
+        if( contentType.equalsIgnoreCase("application/xml")){
+            res = httpRequestUtil.postRequest(getRequestUrl(), getCreateInvoiceAction(secureId,contractNumber,reqParam));
+        }else if(contentType.equalsIgnoreCase("application/json")){
+            String reqXml = xmlConvertUtil.jsonToXML(reqParam);
+            reqXml = getCreateInvoiceAction(secureId,contractNumber,reqXml);
+            res = httpRequestUtil.postRequest(getRequestUrl(), reqXml);
+        }
+        return xmlConvertUtil.xmlToJson(res);
+    }
+
+    private String getCreateInvoiceAction(String sId,String icn,String dataBody) {
+        String rootXml = "<root xmlns=\"http://spsr.ru/webapi/xmlconverter/1.3\">\n" +
+                " <Params Name=\"WAXmlConverter\" Ver=\"1.3\" xmlns=\"http://spsr.ru/webapi/WA/1.0\" />\n" +
+                " <Login SID=\"" + sId + "\"/>\n" +
+                " <XmlConverter> \n" +
+                " <GeneralInfo ContractNumber=\"" + icn + "\">\n" +
+                "\n" +
+                dataBody + "\n" +
+                " </GeneralInfo>\n" +
+                "</XmlConverter>\n" +
+                "</root>";
+        return rootXml;
     }
 
     @ApiOperation(value = "Json转换XML",notes = "Json格式转换到XML格式的接口方法")
